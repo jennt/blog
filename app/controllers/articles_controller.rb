bb288d85b12f1350e0d2641726495826963dce68
Jenn Tustin <jenn.tustin@gmail.com>
2016-10-27 16:28:58 -0400

    added update and delete methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,44 @@ http_basic_authenticate_with name: "Ralph", password: "password", except: [:inde
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article].inspect
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
